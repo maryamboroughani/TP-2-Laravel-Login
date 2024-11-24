f8a5e75e2bdce2e6800f3f939fffbc685e94d35d
MaryamBoroughani <maryamboroughani@gmail.com>
2024-11-24 15:25:40 -0500

    update

--- resources/views/etudiants/create.blade.php
+++ resources/views/etudiants/create.blade.php
@@ -5,35 +5,53 @@
 @section('content')
 <div class="container">
     <h1 class="my-4">Ajouter un Nouvel Étudiant</h1>
+
+    <!-- Display validation errors -->
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="{{ route('etudiants.store') }}" method="POST" class="row g-3">
         @csrf
         <div class="col-md-6">
             <label for="nom" class="form-label">Nom</label>
-            <input type="text" class="form-control" id="nom" name="nom" required>
+            <input type="text" class="form-control" id="nom" name="nom" value="{{ old('nom') }}" required>
         </div>
         <div class="col-md-6">
             <label for="adresse" class="form-label">Adresse</label>
-            <input type="text" class="form-control" id="adresse" name="adresse" required>
+            <input type="text" class="form-control" id="adresse" name="adresse" value="{{ old('adresse') }}" required>
         </div>
         <div class="col-md-6">
             <label for="telephone" class="form-label">Téléphone</label>
-            <input type="text" class="form-control" id="telephone" name="telephone" required>
+            <input type="text" class="form-control" id="telephone" name="telephone" value="{{ old('telephone') }}" required>
         </div>
         <div class="col-md-6">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" required>
+            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
         </div>
         <div class="col-md-6">
             <label for="date_de_naissance" class="form-label">Date de Naissance</label>
-            <input type="date" class="form-control" id="date_de_naissance" name="date_de_naissance" required>
+            <input type="date" class="form-control" id="date_de_naissance" name="date_de_naissance" value="{{ old('date_de_naissance') }}" required>
         </div>
         <div class="col-md-6">
             <label for="ville_id" class="form-label">Ville</label>
             <select class="form-select" id="ville_id" name="ville_id" required>
-                @foreach($villes as $ville)
-                    <option value="{{ $ville->id }}">{{ $ville->nom }}</option>
-                @endforeach
-            </select>
+    <option value="">Sélectionnez une ville</option>
+    @foreach($villes as $ville)
+
+        <option value="{{ $ville->id }}" {{ old('ville_id') == $ville->id ? 'selected' : '' }}>
+            {{ $ville->nom }}
+        </option>
+    @endforeach
+</select>
+
+
         </div>
         <div class="col-12">
             <button type="submit" class="btn btn-primary">Enregistrer</button>
